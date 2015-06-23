50d746904066c99811368f9f1f35c16facca111f
Jacek Chmiel <jacek@kontakt.io>
2015-06-23 15:02:51 +0200

    added if defined KT_OWN_COMPILE_FLAGS to add_exe/lib

--- cmtools.cmake
+++ cmtools.cmake
@@ -1,15 +1,17 @@
 # Creates library with the same syntax as command 'add_executable' but 
-# adds KT_OWN_CODE_COMPILE_FLAGS to COMPILE_FLAGS property.
-# use --warn-uninitialized to warn when used with KT_OWN_CODE_COMPILE_FLAGS undefined
+# adds OWN_CODE_COMPILE_FLAGS to COMPILE_FLAGS property.
 function(cmtools_add_executable target_name)
     add_executable(${target_name} ${ARGN}) 
-    set_target_properties(${target_name} PROPERTIES COMPILE_FLAGS ${KT_OWN_CODE_COMPILE_FLAGS})
+    if(DEFINED OWN_CODE_COMPILE_FLAGS)
+        set_target_properties(${target_name} PROPERTIES COMPILE_FLAGS ${OWN_CODE_COMPILE_FLAGS})
+    endif()
 endfunction()
 
 # Creates library with the same syntax as command 'add_library' but 
-# adds KT_OWN_CODE_COMPILE_FLAGS to COMPILE_FLAGS property.
-# use --warn-uninitialized to warn when used with KT_OWN_CODE_COMPILE_FLAGS undefined
+# adds OWN_CODE_COMPILE_FLAGS to COMPILE_FLAGS property.
 function(cmtools_add_library target_name)
     add_library(${target_name} ${ARGN}) 
-    set_target_properties(${target_name} PROPERTIES COMPILE_FLAGS ${KT_OWN_CODE_COMPILE_FLAGS})
+    if(DEFINED OWN_CODE_COMPILE_FLAGS)
+        set_target_properties(${target_name} PROPERTIES COMPILE_FLAGS ${OWN_CODE_COMPILE_FLAGS})
+    endif()
 endfunction()
